bd14fead5ca69a00da09dd79602b25c503d65b82
Silvia Becerra <silvibecerra681@gmail.com>
2022-12-14 12:45:58 -0500

    Archivo base test

---
+++ demo-sb2/src/test/java/ejempl/example/demosb2/DemoSb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ejempl.example.demosb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
