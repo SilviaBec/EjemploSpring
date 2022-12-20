a804e49b6619a99ed61b2e2ddbee793edd9addf2
Silvia Becerra <silvibecerra681@gmail.com>
2022-12-19 19:09:14 -0500

    Actualización app.js

--- demo-sb2/src/main/resources/static/js/app.js
+++ demo-sb2/src/main/resources/static/js/app.js
@@ -1,6 +1,16 @@
 function abrirCalculadora(){
     const main = document.getElementsByTagName("main").item(0)
-    console.log("main:", main)
-    main.innerHTML ="<h1>Calculadora</h1>"
+    // console.log("main:", main)
+    let body = "<h1>Calculadora</h1>";
+    body += "<form>";
+    body += "<div class='row'>";
+    body += "<div class='col'><input name='num1' type='number' placeholder='Numero 1' required></div>";
+    body += "<select></select>"
+    body += "<div class='col'><input name='num2' type='number' placeholder='Numero 2' required></div>";
+    body += "</div>";
+    body += "</form>";
+
+    main.innerHTML =body;
+                                                                            
 }
 
